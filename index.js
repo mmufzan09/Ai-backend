d976cd4e47a9f443d4a4791bdfe1216d0f1cdacd
MuhammadMufzan <muhammadmufzan787@gmail.com>
2025-09-07 23:37:30 +0500

    hii

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import geminiResponse from "./gemini.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // ✅ Middlewares
 app.use(cors({
